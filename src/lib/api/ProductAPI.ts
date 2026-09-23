490483244f5f747d5f504466508997e9fecd113e
jwjooth <185214226+jwjooth@users.noreply.github.com>
2026-09-23 10:29:38 +0700

    feat: call api development for product

---
+++ src/lib/api/ProductAPI.ts
@@ -0,0 +1,24 @@
+import type {Pagination, ProductRequest, ProductResponse} from "@/lib/props/ProductProps.ts";
+import {fetchDeleteHelper, fetchGetHelper, fetchPostHelper, fetchUpdateHelper} from "@/lib/utils/fetchHelper.ts";
+
+const API_BASE_URL = process?.env?.API_BASE_URL
+
+export const getAllProduct = async (page: number, limit: number): Promise<Pagination<ProductResponse>> => {
+    return (await fetch(`${API_BASE_URL}/products?page=${page}&limit=${limit}`, fetchGetHelper())).json()
+}
+
+const getProductById = async (id: number): Promise<ProductResponse> => {
+    return (await fetch(`${API_BASE_URL}/products/${id}`, fetchGetHelper())).json()
+}
+
+const createProduct = async (request: ProductRequest): Promise<ProductResponse> => {
+    return (await fetch(`${API_BASE_URL}/products`, fetchPostHelper(request))).json()
+}
+
+const updateProduct = async (id: number, request: ProductRequest): Promise<ProductResponse> => {
+    return (await fetch(`${API_BASE_URL}/producsts/${id}`, fetchUpdateHelper(request))).json()
+}
+
+const deleteProduct = async (id: number): Promise<{ message: string }> => {
+    return (await fetch(`${API_BASE_URL}/products/${id}`, fetchDeleteHelper())).json()
+}
